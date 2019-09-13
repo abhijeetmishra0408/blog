226716e71a31099b9545f8cdf8ca59456b4ba784
mishraabhijeet <abhijeetmishra0408@gmail.com>
2019-09-13 17:21:15 +0530

    Commit test

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -5,7 +5,7 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     body = models.TextField()
-    date = models.DateTimeField(auto_now_add = True)
+    # date = models.DateTimeField(auto_now_add = True)
     # add in thumbnail later
     # add in author later
 
